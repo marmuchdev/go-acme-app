83fa7ee2a293ff90e7450a55443e55dee20b172f
Marcin Mucha <marcin.mucha@bjss.com>
2024-09-19 18:58:33 +0100

    modify product model

--- model/product.go
+++ model/product.go
@@ -4,5 +4,5 @@ type Product struct {
 	ID          int     `json:"id" db:"id"`
 	Name        string  `json:"name" db:"name"`
 	Price       float32 `json:"price" db:"price"`
-	Stock_count string  `json:"stock_count" db:"stock_count"`
+	Stock_count int     `json:"stock_count" db:"stock_count"`
 }
